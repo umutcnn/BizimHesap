37895b3c91c0d07643c44dd41acf8cfea6d43017
Burak Hamzaoğlu <burakh16@hotmail.com>
2019-02-07 17:46:27 +0300

    Add project files.

---
+++ BizimHesap/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BizimHesap
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
